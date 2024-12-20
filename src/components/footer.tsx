df6f20990beb843455d81c7933937dca816f188b
gutemonmon052 <herryliukae052@gmail.com>
2024-12-20 17:52:02 +0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer () {
     <div>
       <div className="hlfooter">
         <div className="logo-white">
-          <img src="../assets/images/white-logo.png" srcSet='../assets/images/white-logo.png' sizes="(min-width: 780px) 80px 40px" alt="" />
+          <img src="../assets/images/white-logo.png" srcSet='../assets/images/white-logo.png' sizes="(min-width: 780px) 80px, 40px" alt="" />
         </div>
         <div className="copyright">
            <span>Copyright &copy; 2024 Herry Liukae</span>
