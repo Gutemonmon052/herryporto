3019a1dfc568d0d57f2f31c4b17d538089ca7735
gutemonmon052 <herryliukae052@gmail.com>
2024-12-20 18:13:41 +0800

    Update jumbotron.tsx

--- src/components/jumbotron.tsx
+++ src/components/jumbotron.tsx
@@ -14,7 +14,13 @@ export function Jumbotron() {
           </div>
           <div className="jumbotron-image">
             <div className="cover"></div>
-            <img src="/assets/images/heropic-2.jpg" srcSet="/assets/images/heropic-2-300x450.jpg, /assets/images/heropic-2-721×1082.jpg, /assets/images/heropic-2-920x1380.jpg, /assets/images/heropic-2-1080x1620.jpg, /assets/images/heropic-2-1260x1820.jpg, /assets/images/heropic-2-1420x2130.jpg, /assets/images/heropic-2-1560x2340.jpg, /assets/images/heropic-2-1560x2340.jpg, /assets/images/heropic-2-1690x2535.jpg, /assets/images/heropic-2-1810x2715.jpg, /assets/images/heropic-2-1920x2880.jpg, /assets/images/heropic-2-1990x2985.jpg, /assets/images/heropic-2-2048x3072.jpg" sizes="(max-width: 300px) 100vw, (max-width: 720px) 50vw, (max-width: 1080px) 33vw, 25vw" alt="hero-jumbotron" />          </div>
+            <img
+              src="/assets/images/heropic-2.jpg"
+              srcSet="/assets/images/heropic-2-300x450.jpg 300w, /assets/images/heropic-2-721×1082.jpg 721w, /assets/images/heropic-2-920x1380.jpg 920w, /assets/images/heropic-2-1080x1620.jpg 1080w, /assets/images/heropic-2-1260x1820.jpg 1260w, /assets/images/heropic-2-1420x2130.jpg 1420w, /assets/images/heropic-2-1560x2340.jpg 1560w, /assets/images/heropic-2-1560x2340.jpg 2340w, /assets/images/heropic-2-1690x2535.jpg 1690w, /assets/images/heropic-2-1810x2715.jpg 1810w, /assets/images/heropic-2-1920x2880.jpg 2880w, /assets/images/heropic-2-1990x2985.jpg 1990w, /assets/images/heropic-2-2048x3072.jpg 2048w"
+              sizes="100vw"
+              alt="hero-jumbotron"
+            />{" "}
+          </div>
           <div className="jumbotron-identity">
             <div className="jumbotron-iden-desc">
               <div className="jumbotron-iden-desc-1">
@@ -35,8 +41,7 @@ export function Jumbotron() {
             </div>
           </div>
         </div>
-        <div>
-        </div>
+        <div></div>
       </div>
     </div>
   );
