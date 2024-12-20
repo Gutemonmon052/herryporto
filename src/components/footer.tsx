6a55448032cffdcac3486ad7e55e880c7f3d4d37
gutemonmon052 <herryliukae052@gmail.com>
2024-12-20 09:31:08 +0800

    feat : update image

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer () {
     <div>
       <div className="hlfooter">
         <div className="logo-white">
-          <img src="../assets/images/white-logo.svg" alt="" />
+          <img src="../assets/images/white-logo.png" alt="" />
         </div>
         <div className="copyright">
            <span>Copyright &copy; 2024 Herry Liukae</span>
