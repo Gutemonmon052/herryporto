809371fcc353f49f62daf3c88ad1333d5f716c47
gutemonmon052 <herryliukae052@gmail.com>
2024-12-20 18:08:05 +0800

    Update jumbotron.tsx

--- src/components/jumbotron.tsx
+++ src/components/jumbotron.tsx
@@ -14,8 +14,7 @@ export function Jumbotron() {
           </div>
           <div className="jumbotron-image">
             <div className="cover"></div>
-            <img src="/assets/images/heropic-2.webp" srcSet="/assets/images/heropic-2-300x450.webp, /assets/images/heropic-2-721×1082.webp, /assets/images/heropic-2-920x1380.webp, /assets/images/heropic-2-1080x1620.webp, /assets/images/heropic-2-1260x1820.webp, /assets/images/heropic-2-1420x2130.webp, /assets/images/heropic-2-1560x2340.webp, /assets/images/heropic-2-1560x2340.webp, /assets/images/heropic-2-1690x2535.webp, /assets/images/heropic-2-1810x2715.webp, /assets/images/heropic-2-1920x2880.webp, /assets/images/heropic-2-1990x2985.webp, /assets/images/heropic-2-2048x3072.webp" alt="hero-jumbotron" />
-          </div>
+            <img src="/assets/images/heropic-2.jpg" srcSet="/assets/images/heropic-2-300x450.jpg, /assets/images/heropic-2-721×1082.jpg, /assets/images/heropic-2-920x1380.jpg, /assets/images/heropic-2-1080x1620.jpg, /assets/images/heropic-2-1260x1820.jpg, /assets/images/heropic-2-1420x2130.jpg, /assets/images/heropic-2-1560x2340.jpg, /assets/images/heropic-2-1560x2340.jpg, /assets/images/heropic-2-1690x2535.jpg, /assets/images/heropic-2-1810x2715.jpg, /assets/images/heropic-2-1920x2880.jpg, /assets/images/heropic-2-1990x2985.jpg, /assets/images/heropic-2-2048x3072.jpg" sizes="(max-width: 300px) 100vw, (max-width: 720px) 50vw, (max-width: 1080px) 33vw, 25vw" alt="hero-jumbotron" />          </div>
           <div className="jumbotron-identity">
             <div className="jumbotron-iden-desc">
               <div className="jumbotron-iden-desc-1">
