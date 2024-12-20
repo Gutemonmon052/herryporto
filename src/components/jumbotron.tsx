3a05c8a755bb7c63e69df679cb4bde4e5c3a1935
gutemonmon052 <herryliukae052@gmail.com>
2024-12-20 19:33:27 +0800

    feat : update style

--- src/components/jumbotron.tsx
+++ src/components/jumbotron.tsx
@@ -16,7 +16,6 @@ export function Jumbotron() {
             <div className="cover"></div>
             <img
               src="/assets/images/heropic-2.webp"
-              srcSet="/assets/images/heropic-2-300x450.webp 300w, /assets/images/heropic-2-721×1082.webp 721w, /assets/images/heropic-2-920x1380.webp 920w, /assets/images/heropic-2-1080x1620.webp 1080w, /assets/images/heropic-2-1260x1820.webp 1260w, /assets/images/heropic-2-1420x2130.webp 1420w, /assets/images/heropic-2-1560x2340.webp 1560w, /assets/images/heropic-2-1560x2340.webp 2340w, /assets/images/heropic-2-1690x2535.webp 1690w, /assets/images/heropic-2-1810x2715.webp 1810w, /assets/images/heropic-2-1920x2880.webp 2880w, /assets/images/heropic-2-1990x2985.webp 1990w, /assets/images/heropic-2-2048x3072.webp 2048w"
               sizes="100vw"
               alt="hero-jumbotron"
             />{" "}
