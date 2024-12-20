eae1e44863119bc0a576eb682e411e344222e9dc
gutemonmon052 <herryliukae052@gmail.com>
2024-12-20 18:02:46 +0800

    Update jumbotron.tsx

--- src/components/jumbotron.tsx
+++ src/components/jumbotron.tsx
@@ -14,7 +14,7 @@ export function Jumbotron() {
           </div>
           <div className="jumbotron-image">
             <div className="cover"></div>
-            <img src="/assets/images/heropic-2.webp" alt="hero-jumbotron" />
+            <img src="/assets/images/heropic-2.webp" srcSet="/assets/images/heropic-2-300x450.webp, /assets/images/heropic-2-721×1082.webp, /assets/images/heropic-2-920x1380.webp, /assets/images/heropic-2-1080x1620.webp, /assets/images/heropic-2-1260x1820.webp, /assets/images/heropic-2-1420x2130.webp, /assets/images/heropic-2-1560x2340.webp, /assets/images/heropic-2-1560x2340.webp, /assets/images/heropic-2-1690x2535.webp, /assets/images/heropic-2-1810x2715.webp, /assets/images/heropic-2-1920x2880.webp, /assets/images/heropic-2-1990x2985.webp, /assets/images/heropic-2-2048x3072.webp" alt="hero-jumbotron" />
           </div>
           <div className="jumbotron-identity">
             <div className="jumbotron-iden-desc">
